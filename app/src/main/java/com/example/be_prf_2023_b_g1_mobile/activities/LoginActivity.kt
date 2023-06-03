ce5367299772649cbacc6b2329eb487291f26eb0
Ezequiel Hoyos <ezequielhoyos@outlook.com>
2023-06-02 23:35:24 -0300

    Pantalla de Login

---
+++ app/src/main/java/com/example/be_prf_2023_b_g1_mobile/activities/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.be_prf_2023_b_g1_mobile.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.be_prf_2023_b_g1_mobile.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener(){
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
