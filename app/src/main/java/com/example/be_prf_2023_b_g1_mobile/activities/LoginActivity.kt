5cc71a0c798b43c0f8afcdbfb8ff95b464e0be52
Ezequiel Hoyos <ezequielhoyos@outlook.com>
2023-06-03 21:29:56 -0300

    Adicion de token

--- app/src/main/java/com/example/be_prf_2023_b_g1_mobile/activities/LoginActivity.kt
+++ app/src/main/java/com/example/be_prf_2023_b_g1_mobile/activities/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.example.be_prf_2023_b_g1_mobile.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.jwt.JWT
 import com.example.be_prf_2023_b_g1_mobile.R
 
 class LoginActivity : AppCompatActivity() {
@@ -13,9 +13,11 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
+
         val btnLogin = findViewById<Button>(R.id.btn_login)
         btnLogin.setOnClickListener() {
             val intent = Intent(this@LoginActivity, MainActivity::class.java)
+
             startActivity(intent)
         }
     }
