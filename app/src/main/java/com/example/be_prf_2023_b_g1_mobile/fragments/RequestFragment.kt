810ddb4b094e839fbd14e94c1fe4f179e65dbb0b
Ezequiel Hoyos <ezequielhoyos@outlook.com>
2023-06-04 18:49:47 -0300

    Cambio de nombre fragment details

--- app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/RequestFragment.kt
+++ app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/RequestFragment.kt
@@ -35,7 +35,7 @@ class RequestFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request, container, false)
+        return inflater.inflate(R.layout.fragment_request_details, container, false)
     }
 
     companion object {
