c6ad328e6fe86347a0e8ad8c7676ca0f984a83b1
PseudoEmpirico <facundopradells@gmail.com>
2023-06-05 15:07:22 -0300

    Revert "Merge branch 'dev' into link_details"
    This reverts commit aa51938cfb04de599fe8a4ffc351e76d3738bce7, reversing
    changes made to 9ab08f5bfabd96f76d6b8e4cf73e7ba4dc9498ee.

--- app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/RequestsFragment.kt
+++ app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/RequestsFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.be_prf_2023_b_g1_mobile.APIServiceBuilder.APIServiceBuilder
@@ -14,7 +13,6 @@ import com.example.be_prf_2023_b_g1_mobile.APIServiceBuilder.RequestService
 import com.example.be_prf_2023_b_g1_mobile.R
 import com.example.be_prf_2023_b_g1_mobile.model.RequestResponse
 import com.example.be_prf_2023_b_g1_mobile.recycleViewAdapter.RequestAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,18 +25,12 @@ class RequestsFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         thisView = inflater.inflate(R.layout.fragment_requests, container, false)
         recycleRequests = thisView.findViewById(R.id.recycleRequests)
-
-        val  btnGoToNewRequest = thisView.findViewById<FloatingActionButton>(R.id.goToNewRequest)
-
-        btnGoToNewRequest.setOnClickListener{
-
-            val action = RequestsFragmentDirections.actionRequestsFragmentToNewRequestFragment()
-            thisView.findNavController().navigate(action)
-        }
-
         val service = APIServiceBuilder.createRequestService()
 
         service.getRequests().enqueue(
--- app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/StationsFragment.kt
+++ app/src/main/java/com/example/be_prf_2023_b_g1_mobile/fragments/StationsFragment.kt
@@ -6,14 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.be_prf_2023_b_g1_mobile.APIServiceBuilder.APIServiceBuilder
 import com.example.be_prf_2023_b_g1_mobile.model.StationResponse
 import com.example.be_prf_2023_b_g1_mobile.R
 import com.example.be_prf_2023_b_g1_mobile.recycleViewAdapter.StationAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,18 +23,12 @@ class StationsFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         thisView = inflater.inflate(R.layout.fragment_stations, container, false)
         recycleStations = thisView.findViewById(R.id.recycleStations)
-
-        val  btnGoToNewRequest = thisView.findViewById<FloatingActionButton>(R.id.goToNewRequest)
-
-        btnGoToNewRequest.setOnClickListener{
-
-            val action = StationsFragmentDirections.actionStationsFragmentToNewRequestFragment()
-            thisView.findNavController().navigate(action)
-        }
-
         val service = APIServiceBuilder.createStationService()
 
         service.getStations().enqueue(
